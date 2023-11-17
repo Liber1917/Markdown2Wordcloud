55e5ead84f8fd0a7f49b729a294435cf9f9b3c73
Carl <108458660+Liber1917@users.noreply.github.com>
2023-11-17 09:08:21 +0800

    尝试conda虚拟环境

--- set_up.bat
+++ set_up.bat
@@ -12,11 +12,11 @@ if %errorlevel% equ 0 (
     REM 当前 Python 版本不是 3.11，创建虚拟环境
     echo Current Python version is not %PYTHON_VERSION%, creating virtual environment
 
-    REM 创建虚拟环境
-    python -m venv myenv
+    REM 创建 conda 虚拟环境
+    conda create -n myenv python=%PYTHON_VERSION%
 
     REM 激活虚拟环境
-    call myenv\Scripts\activate
+    call activate myenv
 )
 
 REM 安装依赖项
@@ -28,5 +28,4 @@ pip install mistune
 
 REM 其他操作...
 
-REM 退出虚拟环境
-deactivate
+
